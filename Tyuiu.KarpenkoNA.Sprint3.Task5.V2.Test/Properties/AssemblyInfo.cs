bbd7ded7232f4a0d536a917f74c7adddfb7624c8
Tyuiu.KarpenkoNA@outlook.com <karpenko.nik05@mail.ru>
2023-10-25 12:53:39 +0500

    Task5

---
+++ Tyuiu.KarpenkoNA.Sprint3.Task5.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KarpenkoNA.Sprint3.Task5.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KarpenkoNA.Sprint3.Task5.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1fcd3f1e-8ae0-4479-b51d-bbd53986818e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
